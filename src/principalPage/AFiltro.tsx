e773e05549412303a7272261a7325b7e9a46571f
xxXleitoXxx <navartaleonel@gmail.com>
2025-05-20 15:19:07 +0000

    Alta Alumno integrado en abm

--- src/principalPage/AFiltro.tsx
+++ src/principalPage/AFiltro.tsx
@@ -1,9 +1,36 @@
-import { Button } from "react-bootstrap";
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import AltaAlumno from "../assets/AbmAlumno/AltaAlumno";
 
 export default function AFiltro() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="d-flex align-items-end">
-      <Button variant="success">Agregar Alumno</Button>
+      <Button variant="success" onClick={() => setShow(true)}>
+        Agregar Alumno
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-lg"
+        aria-labelledby="example-custom-modal-styling-title"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Alta Alumno
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            <AltaAlumno />
+          </p>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
